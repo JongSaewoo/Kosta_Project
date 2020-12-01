5c627617d2eaa5a3ff8c14e65e2fbac974dc2acf
JongSaewoo <51853003+JongSaewoo@users.noreply.github.com>
2020-12-01 16:37:44 +0900

    CREATE users TABLE

--- NoTimeToDie/Assets/Scenes/Scripts/WebSripts/Web.cs
+++ NoTimeToDie/Assets/Scenes/Scripts/WebSripts/Web.cs
@@ -14,8 +14,99 @@ public class Web : MonoBehaviour
         // user_name, user_password 입력
         //StartCoroutine(Login("JongWoo", "1234"));
 
-        //StartCoroutine(ResisterUser("SeongMin", "123456"));
+        StartCoroutine(ResisterUser("SeongMin", "123456"));
     }
 
-    
+    // GetDate 데이터 파일
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityStartLogin/GetDate.php"))
+        {
+            yield return www.Send();
+
+            // 에러 페이지
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // 결과 텍스트를 보여줌
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    // GetUsers 데이터 파일
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityStartLogin/GetUsers.php"))
+        {
+            yield return www.Send();
+
+            // 에러 페이지
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // GetUsers.php에서 출력한 데이터의 결과 텍스트 값을 가져옴 
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    IEnumerator Login(string user_name, string user_password)
+    {
+        WWWForm form = new WWWForm();
+        // Login.php에서 $loginUser와 $loginPass에 
+        // Post한 값을 Get : 'loginUser' 
+        form.AddField("loginUser", user_name);
+        form.AddField("loginPass", user_password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/UnityStartLogin/Login.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            // 에러 페이지
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Login.php에서 출력한 데이터 값
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+    IEnumerator ResisterUser(string user_name, string user_password)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("loginUser", user_name);
+        form.AddField("loginPass", user_password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/UnityStartLogin/RegisterUser.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            // 에러 페이지
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Login.php에서 출력한 데이터 값
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
 }
