e668c672b4fe990e9cfd39f3395e40ff663dbda9
JongSaewoo <51853003+JongSaewoo@users.noreply.github.com>
2020-12-01 16:33:40 +0900

    db

--- NoTimeToDie/Assets/Scenes/Scripts/WebSripts/Web.cs
+++ NoTimeToDie/Assets/Scenes/Scripts/WebSripts/Web.cs
@@ -8,57 +8,14 @@ public class Web : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetDate());
-        StartCoroutine(GetUsers());
+        //StartCoroutine(GetDate());
+        //StartCoroutine(GetUsers());
+
+        // user_name, user_password 입력
+        //StartCoroutine(Login("JongWoo", "1234"));
+
+        //StartCoroutine(ResisterUser("SeongMin", "123456"));
     }
 
-    // GetDate 데이터 파일
-    IEnumerator GetDate()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityStartLogin/GetDate.php")) 
-        {
-            yield return www.Send();
-
-            // 에러 페이지
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                // 결과 텍스트를 보여줌
-                Debug.Log(www.downloadHandler.text);
-               
-                byte[] results = www.downloadHandler.data;
-            }
-
-
-        }
-    }
-
-    // GetUsers 데이터 파일
-    IEnumerator GetUsers()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityStartLogin/GetUsers.php"))
-        {
-            yield return www.Send();
-
-            // 에러 페이지
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                // 결과 텍스트를 보여줌
-                Debug.Log(www.downloadHandler.text);
-
-                byte[] results = www.downloadHandler.data;
-            }
-
-
-        }
-    }
-
-
+    
 }
