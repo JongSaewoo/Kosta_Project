a1307c5d269005a503e2a0bd235dcb2215a03052
JongSaewoo <51853003+JongSaewoo@users.noreply.github.com>
2021-01-04 16:43:12 +0900

    최종완성

---
+++ VR_Project/Assets/Scripts/007/Gameover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("bullet"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            //SceneManager.LoadSceneAsync("Login");
+        }
+    }
+}
